ecbeffc79584daec76cbc5e1b01ba0c3269a8e2f
Trevor Thompson <Trevor.Ray.Thompson@gmail.com>
2020-01-23 16:30:23 -0800

    added response messages

--- src/auth/additional-routes.js
+++ src/auth/additional-routes.js
@@ -8,28 +8,28 @@ const acl = require('./acl-middleware');
 router.get('/public');
 
 router.get('/private', bearAuth, (req, res, next) => {
-
+  res.send('OK');
 }); 
 
 router.get('/readonly', bearAuth, acl('read'), (req, res, next) => {
-
+  res.send('OK');
 }); 
 
 router.get('/create', bearAuth, acl('create'), (req, res, next) => {
-
+  res.send('OK');
 });
 
 router.put('/update', bearAuth, acl('update'), (req, res, next) => {
-
+  res.send('OK');
 }); 
 
 
 router.patch('/delete', bearAuth, acl('delete'), (req, res, next) => {
-
+  res.send('OK');
 });
 
 router.get('/everything', bearAuth, acl('superuser'), (req, res, next) => {
-
+  res.send('OK');
 });
 
 module.exports = router;
